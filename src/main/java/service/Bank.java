46cbeb28c3b2910d0d030cd6a4120a980fead50d
Lim DongGyu <ldg1160296@gmail.com>
2022-11-24 17:54:06 +0900

    다시 올려보는 브런치

--- src/main/java/service/Bank.java
+++ src/main/java/service/Bank.java
@@ -1,2 +1,34 @@
-package service;public class Bank {
+package service;
+
+public class Bank {
+
+
+    //계좌등록 부분(유저 이름, 비밀번호, 계좌번호
+    private static void createAccount() {
+
+    }
+    //계좌목록조회
+    private static void accountList() {
+
+    }
+    //입금
+    private static void deposit() {
+
+    }
+    //출금
+    private static void withdraw() {
+
+    }
+    //정보수정
+    private static void updateAccount(){
+
+    }
+    //계좌삭제
+    private static void deleteAccount(){
+
+    }
+    //계좌내역
+    private static void accountDataList(){
+
+    }
 }
